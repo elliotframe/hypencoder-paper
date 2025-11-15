b3e8bd8c58c1e852ec17fa233ee42491c0afe055
2735258f <elliot.frame@gmail.com>
2025-11-15 12:59:25 +0000

    Insert breakpoints

--- hypencoder_cb/inference/approx_retrieve.py
+++ hypencoder_cb/inference/approx_retrieve.py
@@ -239,10 +239,11 @@ class HypecoderGraphRetriever(BaseRetriever):
             candidate_embeddings = candidate_embeddings.unsqueeze(0)
             if not callable(query_model):
                 raise TypeError(f"Expected query_model to be callable, got {type(query_model)}")
+            breakpoint()
             similarity_matrix = query_model(candidate_embeddings).squeeze()
             # similarity_matrix = query_model(candidate_embeddings)
             # similarity_matrix = similarity_matrix.view(-1)
-
+            breakpoint()
             ncandidates = min(
                 max(self.ncandidates, top_k), similarity_matrix.shape[0]
             )
