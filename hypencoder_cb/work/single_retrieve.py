1048a979f21814b28afaa8b6bc226fab47b90bf5
2735258f <elliot.frame@gmail.com>
2025-12-11 19:04:25 +0000

    Less entry points

--- hypencoder_cb/work/single_retrieve.py
+++ hypencoder_cb/work/single_retrieve.py
@@ -47,7 +47,7 @@ def main(
 
 
 
-    num_entry_points = 1000
+    num_entry_points = 5000
     ncandidates = 64
     max_iter = 16
     metric_dir=f"metrics/{ret_name}/entries/{num_entry_points}-{ncandidates}-{max_iter}"
