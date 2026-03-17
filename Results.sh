33e1086d8f6bfc0a2e0ebc219a19e1780446d29f
2735258f <2735258f@student.gla.ac.uk>
2026-03-17 15:18:01 +0000

    Remaining results

--- Results.sh
+++ Results.sh
@@ -1,151 +1,151 @@
 
-# Exhaustive 2019
-export MODEL_NAME_OR_PATH="jfkback/hypencoder.6_layer"
-export ENCODING_PATH="~/nfs/hypencoder-paper/encodings/msmarco"
-export RETRIEVAL_DIR="retrievals/exhaustive/in-domain/2019"
-export IR_DATASET_NAME="msmarco-passage/trec-dl-2019/judged"
-export METRIC_NAME="metrics/exhaustive/in-domain/2019"
+# # Exhaustive 2019
+# export MODEL_NAME_OR_PATH="jfkback/hypencoder.6_layer"
+# export ENCODING_PATH="~/nfs/hypencoder-paper/encodings/msmarco"
+# export RETRIEVAL_DIR="retrievals/exhaustive/in-domain/2019"
+# export IR_DATASET_NAME="msmarco-passage/trec-dl-2019/judged"
+# export METRIC_NAME="metrics/exhaustive/in-domain/2019"
 
-python hypencoder_cb/inference/retrieve.py \
---model_name_or_path=$MODEL_NAME_OR_PATH \
---encoded_item_path=$ENCODING_PATH \
---output_dir=$RETRIEVAL_DIR \
---ir_dataset_name=$IR_DATASET_NAME \
---query_max_length=64 \
---metric_dir=$METRIC_NAME
+# python hypencoder_cb/inference/retrieve.py \
+# --model_name_or_path=$MODEL_NAME_OR_PATH \
+# --encoded_item_path=$ENCODING_PATH \
+# --output_dir=$RETRIEVAL_DIR \
+# --ir_dataset_name=$IR_DATASET_NAME \
+# --query_max_length=64 \
+# --metric_dir=$METRIC_NAME
 
-python hypencoder_cb/work/add_timings_exhaustive.py \
---ret_name="exhaustive/in-domain/2019"
-
-
-# Exhaustive 2020
-export MODEL_NAME_OR_PATH="jfkback/hypencoder.6_layer"
-export ENCODING_PATH="~/nfs/hypencoder-paper/encodings/msmarco"
-export RETRIEVAL_DIR="retrievals/exhaustive/in-domain/2020"
-export IR_DATASET_NAME="msmarco-passage/trec-dl-2020/judged"
-export METRIC_NAME="metrics/exhaustive/in-domain/2020"
-
-python hypencoder_cb/inference/retrieve.py \
---model_name_or_path=$MODEL_NAME_OR_PATH \
---encoded_item_path=$ENCODING_PATH \
---output_dir=$RETRIEVAL_DIR \
---ir_dataset_name=$IR_DATASET_NAME \
---query_max_length=64 \
---metric_dir=$METRIC_NAME
-
-python hypencoder_cb/work/add_timings_exhaustive.py \
---ret_name="exhaustive/in-domain/2020"
-
-# Exhaustive dev
-export MODEL_NAME_OR_PATH="jfkback/hypencoder.6_layer"
-export ENCODING_PATH="~/nfs/hypencoder-paper/encodings/msmarco"
-export RETRIEVAL_DIR="retrievals/exhaustive/in-domain/dev"
-export IR_DATASET_NAME="msmarco-passage/dev/small"
-export METRIC_NAME="metrics/exhaustive/in-domain/dev"
-
-python hypencoder_cb/inference/retrieve.py \
---model_name_or_path=$MODEL_NAME_OR_PATH \
---encoded_item_path=$ENCODING_PATH \
---output_dir=$RETRIEVAL_DIR \
---ir_dataset_name=$IR_DATASET_NAME \
---query_max_length=64 \
---metric_dir=$METRIC_NAME
-
-python hypencoder_cb/work/add_timings_exhaustive.py \
---ret_name="exhuastive/in-domain/dev"
+# python hypencoder_cb/work/add_timings_exhaustive.py \
+# --ret_name="exhaustive/in-domain/2019"
 
 
+# # Exhaustive 2020
+# export MODEL_NAME_OR_PATH="jfkback/hypencoder.6_layer"
+# export ENCODING_PATH="~/nfs/hypencoder-paper/encodings/msmarco"
+# export RETRIEVAL_DIR="retrievals/exhaustive/in-domain/2020"
+# export IR_DATASET_NAME="msmarco-passage/trec-dl-2020/judged"
+# export METRIC_NAME="metrics/exhaustive/in-domain/2020"
 
-# Exhaustive fiqaTest
-export MODEL_NAME_OR_PATH="jfkback/hypencoder.6_layer"
-export ENCODING_PATH="~/nfs/hypencoder-paper/encodings/fiqaTest"
-export RETRIEVAL_DIR="retrievals/exhaustive/out-domain/fiqaTest"
-export IR_DATASET_NAME="beir/fiqa/test"
-export METRIC_NAME="metrics/exhaustive/out-domain/fiqaTest"
+# python hypencoder_cb/inference/retrieve.py \
+# --model_name_or_path=$MODEL_NAME_OR_PATH \
+# --encoded_item_path=$ENCODING_PATH \
+# --output_dir=$RETRIEVAL_DIR \
+# --ir_dataset_name=$IR_DATASET_NAME \
+# --query_max_length=64 \
+# --metric_dir=$METRIC_NAME
 
-python hypencoder_cb/inference/retrieve.py \
---model_name_or_path=$MODEL_NAME_OR_PATH \
---encoded_item_path=$ENCODING_PATH \
---output_dir=$RETRIEVAL_DIR \
---ir_dataset_name=$IR_DATASET_NAME \
---query_max_length=512 \
---metric_dir=$METRIC_NAME
+# python hypencoder_cb/work/add_timings_exhaustive.py \
+# --ret_name="exhaustive/in-domain/2020"
 
-python hypencoder_cb/work/add_timings_exhaustive.py \
---ret_name="exhaustive/out-domain/fiqaTest"
+# # Exhaustive dev
+# export MODEL_NAME_OR_PATH="jfkback/hypencoder.6_layer"
+# export ENCODING_PATH="~/nfs/hypencoder-paper/encodings/msmarco"
+# export RETRIEVAL_DIR="retrievals/exhaustive/in-domain/dev"
+# export IR_DATASET_NAME="msmarco-passage/dev/small"
+# export METRIC_NAME="metrics/exhaustive/in-domain/dev"
+
+# python hypencoder_cb/inference/retrieve.py \
+# --model_name_or_path=$MODEL_NAME_OR_PATH \
+# --encoded_item_path=$ENCODING_PATH \
+# --output_dir=$RETRIEVAL_DIR \
+# --ir_dataset_name=$IR_DATASET_NAME \
+# --query_max_length=64 \
+# --metric_dir=$METRIC_NAME
+
+# python hypencoder_cb/work/add_timings_exhaustive.py \
+# --ret_name="exhuastive/in-domain/dev"
 
 
-# Exhaustive covid
-export MODEL_NAME_OR_PATH="jfkback/hypencoder.6_layer"
-export ENCODING_PATH="~/nfs/hypencoder-paper/encodings/treccovid"
-export RETRIEVAL_DIR="retrievals/exhaustive/out-domain/treccovid"
-export IR_DATASET_NAME="beir/trec-covid"
-export METRIC_NAME="metrics/exhaustive/out-domain/treccovid"
 
-python hypencoder_cb/inference/retrieve.py \
---model_name_or_path=$MODEL_NAME_OR_PATH \
---encoded_item_path=$ENCODING_PATH \
---output_dir=$RETRIEVAL_DIR \
---ir_dataset_name=$IR_DATASET_NAME \
---query_max_length=512 \
---metric_dir=$METRIC_NAME
+# # Exhaustive fiqaTest
+# export MODEL_NAME_OR_PATH="jfkback/hypencoder.6_layer"
+# export ENCODING_PATH="~/nfs/hypencoder-paper/encodings/fiqaTest"
+# export RETRIEVAL_DIR="retrievals/exhaustive/out-domain/fiqaTest"
+# export IR_DATASET_NAME="beir/fiqa/test"
+# export METRIC_NAME="metrics/exhaustive/out-domain/fiqaTest"
 
-python hypencoder_cb/work/add_timings_exhaustive.py \
---ret_name="exhaustive/out-domain/treccovid"
+# python hypencoder_cb/inference/retrieve.py \
+# --model_name_or_path=$MODEL_NAME_OR_PATH \
+# --encoded_item_path=$ENCODING_PATH \
+# --output_dir=$RETRIEVAL_DIR \
+# --ir_dataset_name=$IR_DATASET_NAME \
+# --query_max_length=512 \
+# --metric_dir=$METRIC_NAME
 
-# Exhaustive nfcorpus
-export MODEL_NAME_OR_PATH="jfkback/hypencoder.6_layer"
-export ENCODING_PATH="~/nfs/hypencoder-paper/encodings/nfcorpusTest"
-export RETRIEVAL_DIR="retrievals/exhaustive/out-domain/nfcorpus"
-export IR_DATASET_NAME="beir/nfcorpus/test"
-export METRIC_NAME="metrics/exhaustive/out-domain/nfcorpus"
+# python hypencoder_cb/work/add_timings_exhaustive.py \
+# --ret_name="exhaustive/out-domain/fiqaTest"
 
-python hypencoder_cb/inference/retrieve.py \
---model_name_or_path=$MODEL_NAME_OR_PATH \
---encoded_item_path=$ENCODING_PATH \
---output_dir=$RETRIEVAL_DIR \
---ir_dataset_name=$IR_DATASET_NAME \
---query_max_length=512 \
---metric_dir=$METRIC_NAME
 
-python hypencoder_cb/work/add_timings_exhaustive.py \
---ret_name="exhaustive/out-domain/nfcorpus"
+# # Exhaustive covid
+# export MODEL_NAME_OR_PATH="jfkback/hypencoder.6_layer"
+# export ENCODING_PATH="~/nfs/hypencoder-paper/encodings/treccovid"
+# export RETRIEVAL_DIR="retrievals/exhaustive/out-domain/treccovid"
+# export IR_DATASET_NAME="beir/trec-covid"
+# export METRIC_NAME="metrics/exhaustive/out-domain/treccovid"
 
-# Exhaustive dpedia
-export MODEL_NAME_OR_PATH="jfkback/hypencoder.6_layer"
-export ENCODING_PATH="~/nfs/hypencoder-paper/encodings/dpedia-entityTest"
-export RETRIEVAL_DIR="retrievals/exhaustive/out-domain/dpedia"
-export IR_DATASET_NAME="beir/dbpedia-entity/test"
-export METRIC_NAME="metrics/exhaustive/out-domain/dpedia"
+# python hypencoder_cb/inference/retrieve.py \
+# --model_name_or_path=$MODEL_NAME_OR_PATH \
+# --encoded_item_path=$ENCODING_PATH \
+# --output_dir=$RETRIEVAL_DIR \
+# --ir_dataset_name=$IR_DATASET_NAME \
+# --query_max_length=512 \
+# --metric_dir=$METRIC_NAME
 
-python hypencoder_cb/inference/retrieve.py \
---model_name_or_path=$MODEL_NAME_OR_PATH \
---encoded_item_path=$ENCODING_PATH \
---output_dir=$RETRIEVAL_DIR \
---ir_dataset_name=$IR_DATASET_NAME \
---query_max_length=512 \
---metric_dir=$METRIC_NAME
+# python hypencoder_cb/work/add_timings_exhaustive.py \
+# --ret_name="exhaustive/out-domain/treccovid"
 
-python hypencoder_cb/work/add_timings_exhaustive.py \
---ret_name="exhaustive/out-domain/dpedia"
+# # Exhaustive nfcorpus
+# export MODEL_NAME_OR_PATH="jfkback/hypencoder.6_layer"
+# export ENCODING_PATH="~/nfs/hypencoder-paper/encodings/nfcorpusTest"
+# export RETRIEVAL_DIR="retrievals/exhaustive/out-domain/nfcorpus"
+# export IR_DATASET_NAME="beir/nfcorpus/test"
+# export METRIC_NAME="metrics/exhaustive/out-domain/nfcorpus"
 
-# Exhaustive touche
-export MODEL_NAME_OR_PATH="jfkback/hypencoder.6_layer"
-export ENCODING_PATH="~/nfs/hypencoder-paper/encodings/touche2020v2"
-export RETRIEVAL_DIR="retrievals/exhaustive/out-domain/touche"
-export IR_DATASET_NAME="beir/webis-touche2020/v2"
-export METRIC_NAME="metrics/exhaustive/out-domain/touche"
+# python hypencoder_cb/inference/retrieve.py \
+# --model_name_or_path=$MODEL_NAME_OR_PATH \
+# --encoded_item_path=$ENCODING_PATH \
+# --output_dir=$RETRIEVAL_DIR \
+# --ir_dataset_name=$IR_DATASET_NAME \
+# --query_max_length=512 \
+# --metric_dir=$METRIC_NAME
 
-python hypencoder_cb/inference/retrieve.py \
---model_name_or_path=$MODEL_NAME_OR_PATH \
---encoded_item_path=$ENCODING_PATH \
---output_dir=$RETRIEVAL_DIR \
---ir_dataset_name=$IR_DATASET_NAME \
---query_max_length=512 \
---metric_dir=$METRIC_NAME
+# python hypencoder_cb/work/add_timings_exhaustive.py \
+# --ret_name="exhaustive/out-domain/nfcorpus"
 
-python hypencoder_cb/work/add_timings_exhaustive.py \
---ret_name="exhaustive/out-domain/touche"
+# # Exhaustive dpedia
+# export MODEL_NAME_OR_PATH="jfkback/hypencoder.6_layer"
+# export ENCODING_PATH="~/nfs/hypencoder-paper/encodings/dpedia-entityTest"
+# export RETRIEVAL_DIR="retrievals/exhaustive/out-domain/dpedia"
+# export IR_DATASET_NAME="beir/dbpedia-entity/test"
+# export METRIC_NAME="metrics/exhaustive/out-domain/dpedia"
+
+# python hypencoder_cb/inference/retrieve.py \
+# --model_name_or_path=$MODEL_NAME_OR_PATH \
+# --encoded_item_path=$ENCODING_PATH \
+# --output_dir=$RETRIEVAL_DIR \
+# --ir_dataset_name=$IR_DATASET_NAME \
+# --query_max_length=512 \
+# --metric_dir=$METRIC_NAME
+
+# python hypencoder_cb/work/add_timings_exhaustive.py \
+# --ret_name="exhaustive/out-domain/dpedia"
+
+# # Exhaustive touche
+# export MODEL_NAME_OR_PATH="jfkback/hypencoder.6_layer"
+# export ENCODING_PATH="~/nfs/hypencoder-paper/encodings/touche2020v2"
+# export RETRIEVAL_DIR="retrievals/exhaustive/out-domain/touche"
+# export IR_DATASET_NAME="beir/webis-touche2020/v2"
+# export METRIC_NAME="metrics/exhaustive/out-domain/touche"
+
+# python hypencoder_cb/inference/retrieve.py \
+# --model_name_or_path=$MODEL_NAME_OR_PATH \
+# --encoded_item_path=$ENCODING_PATH \
+# --output_dir=$RETRIEVAL_DIR \
+# --ir_dataset_name=$IR_DATASET_NAME \
+# --query_max_length=512 \
+# --metric_dir=$METRIC_NAME
+
+# python hypencoder_cb/work/add_timings_exhaustive.py \
+# --ret_name="exhaustive/out-domain/touche"
 
 
 # Exhaustive 2019 2
@@ -259,16 +259,16 @@ python hypencoder_cb/work/add_timings_exhaustive.py \
 
 
 
-export MODEL_NAME_OR_PATH="jfkback/hypencoder.6_layer"
-export ENCODING_PATH="~/nfs/hypencoder-paper/encodings/msmarco"
-export RETRIEVAL_DIR="retrievals/exhaustive/in-domain/2019"
-export IR_DATASET_NAME="msmarco-passage/trec-dl-2019/judged"
-export ITEM_NEIGHBOR_GRAPH="graphs/msmarco"
+# export MODEL_NAME_OR_PATH="jfkback/hypencoder.6_layer"
+# export ENCODING_PATH="~/nfs/hypencoder-paper/encodings/msmarco"
+# export RETRIEVAL_DIR="retrievals/exhaustive/in-domain/2019"
+# export IR_DATASET_NAME="msmarco-passage/trec-dl-2019/judged"
+# export ITEM_NEIGHBOR_GRAPH="graphs/msmarco"
 
-python hypencoder_cb/work/diff_arguments_final.py \
---model_name_or_path=$MODEL_NAME_OR_PATH \
---encoded_item_path=$ENCODING_PATH \
---item_neighbors_path=$ITEM_NEIGHBOR_GRAPH \
---output_dir=$RETRIEVAL_DIR \
---ir_dataset_name=$IR_DATASET_NAME \
---dtype=bf16
+# python hypencoder_cb/work/diff_arguments_final.py \
+# --model_name_or_path=$MODEL_NAME_OR_PATH \
+# --encoded_item_path=$ENCODING_PATH \
+# --item_neighbors_path=$ITEM_NEIGHBOR_GRAPH \
+# --output_dir=$RETRIEVAL_DIR \
+# --ir_dataset_name=$IR_DATASET_NAME \
+# --dtype=bf16
