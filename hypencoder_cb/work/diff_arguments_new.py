a46a30073488e10ea0e7ba7bb361cdc31051a210
2735258f <2735258f@student.gla.ac.uk>
2026-03-21 21:48:16 +0000

    Test18 onwards

--- hypencoder_cb/work/diff_arguments_new.py
+++ hypencoder_cb/work/diff_arguments_new.py
@@ -590,87 +590,19 @@ def main(
     # -----------
     # test17: Test different k1, b values for rrf with BM25
     # -----------
-    output_dir = "retrievals/test17"
-    ret_name=output_dir.split("/")[-1]
-    k1_values = [0.5, 0.9, 1.2, 1.5, 2.0, 2.5]
-    b_values = [0.3, 0.5, 0.75, 0.8, 1.0]
-    alpha_values = [0.90, 0.91, 0.92, 0.93, 0.94, 0.95, 0.96, 0.97, 0.98, 0.99]
-    num_entry_points = 10000
-    ncandidates = 64
-    max_iter = 16
+    # output_dir = "retrievals/test17"
+    # ret_name=output_dir.split("/")[-1]
+    # k1_values = [0.5, 0.9, 1.2, 1.5, 2.0, 2.5]
+    # b_values = [0.3, 0.5, 0.75, 0.8, 1.0]
+    # alpha_values = [0.90, 0.91, 0.92, 0.93, 0.94, 0.95, 0.96, 0.97, 0.98, 0.99]
+    # num_entry_points = 10000
+    # ncandidates = 64
+    # max_iter = 16
 
 
-    for k1, b, alpha in product(k1_values, b_values, alpha_values):
+    # for k1, b, alpha in product(k1_values, b_values, alpha_values):
 
-        metric_dir=f"metrics/{ret_name}/entries/{k1}-{b}-{alpha}"
-
-        retriever.set_parameters(
-            num_entry_points=num_entry_points,
-            ncandidates=ncandidates,
-            max_iter=max_iter,
-            seed_bm25=False,
-            seed_dph=False,
-            rrf_bm25=True,
-            rrf_dph=False,
-            k1=k1,
-            b=b,
-            alpha=alpha,
-        )
-
-        print(f"Starting retrieval: num_entry_points={num_entry_points}, ncandidates={ncandidates}, max_iter={max_iter}, k1={k1}, b={b}, alpha={alpha}")
-        
-        do_retrieval(
-            retriever=retriever,
-            model_name_or_path=model_name_or_path,
-            encoded_item_path=encoded_item_path,
-            item_neighbors_path=item_neighbors_path,
-            output_dir=output_dir,
-            ir_dataset_name=ir_dataset_name,
-            dtype=dtype,
-            num_entry_points=num_entry_points,
-            ncandidates=ncandidates,
-            max_iter=max_iter,
-            cache_file=cache_file,
-            metric_dir=metric_dir
-        )
-
-
-    print("Combining metrics.")
-    fieldnames = ["k1", "b", "alpha", "NumEntryPoints", "NCandidates", "MaxIter",
-              "P@10", "P@5", "R@10", "R@1000",
-              "RR", "RR@10", "nDCG@10", "nDCG@5"]
-    with open(f"metrics/{ret_name}/results.csv", "w", newline="") as f:
-        writer = csv.DictWriter(f, fieldnames=fieldnames)
-        writer.writeheader()
-        for k1, b, alpha in product(k1_values, b_values, alpha_values):
-            metric_dir=f"metrics/{ret_name}/entries/{k1}-{b}-{alpha}/aggregated_metrics.json"
-
-            with open(metric_dir, "r") as g:
-                metrics = json.load(g)
-
-            row = {"k1": k1, "b":b, "alpha":alpha, "NumEntryPoints": num_entry_points, "NCandidates": ncandidates, "MaxIter": max_iter}
-            row.update(metrics)
-
-            writer.writerow(row)
-    print("Done :)")
-
-
-
-
-    #   # -----------
-    # # test18: Test different alpha values for rrf with dph
-    # # -----------
-    output_dir = "retrievals/test18"
-    ret_name=output_dir.split("/")[-1]
-    alpha_values = [0.90, 0.91, 0.92, 0.93, 0.94, 0.95, 0.96, 0.97, 0.98, 0.99]
-    num_entry_points = 10000
-    ncandidates = 64
-    max_iter = 16
-
-
-    # for alpha in alpha_values:
-
-    #     metric_dir=f"metrics/{ret_name}/entries/{alpha}"
+    #     metric_dir=f"metrics/{ret_name}/entries/{k1}-{b}-{alpha}"
 
     #     retriever.set_parameters(
     #         num_entry_points=num_entry_points,
@@ -678,14 +610,14 @@ def main(
     #         max_iter=max_iter,
     #         seed_bm25=False,
     #         seed_dph=False,
-    #         rrf_bm25=False,
-    #         rrf_dph=True,
+    #         rrf_bm25=True,
+    #         rrf_dph=False,
     #         k1=k1,
     #         b=b,
     #         alpha=alpha,
     #     )
 
-    #     print(f"Starting retrieval: num_entry_points={num_entry_points}, ncandidates={ncandidates}, max_iter={max_iter}, alpha={alpha}")
+    #     print(f"Starting retrieval: num_entry_points={num_entry_points}, ncandidates={ncandidates}, max_iter={max_iter}, k1={k1}, b={b}, alpha={alpha}")
         
     #     do_retrieval(
     #         retriever=retriever,
@@ -701,6 +633,74 @@ def main(
     #         cache_file=cache_file,
     #         metric_dir=metric_dir
     #     )
+
+
+    # print("Combining metrics.")
+    # fieldnames = ["k1", "b", "alpha", "NumEntryPoints", "NCandidates", "MaxIter",
+    #           "P@10", "P@5", "R@10", "R@1000",
+    #           "RR", "RR@10", "nDCG@10", "nDCG@5"]
+    # with open(f"metrics/{ret_name}/results.csv", "w", newline="") as f:
+    #     writer = csv.DictWriter(f, fieldnames=fieldnames)
+    #     writer.writeheader()
+    #     for k1, b, alpha in product(k1_values, b_values, alpha_values):
+    #         metric_dir=f"metrics/{ret_name}/entries/{k1}-{b}-{alpha}/aggregated_metrics.json"
+
+    #         with open(metric_dir, "r") as g:
+    #             metrics = json.load(g)
+
+    #         row = {"k1": k1, "b":b, "alpha":alpha, "NumEntryPoints": num_entry_points, "NCandidates": ncandidates, "MaxIter": max_iter}
+    #         row.update(metrics)
+
+    #         writer.writerow(row)
+    # print("Done :)")
+
+
+
+
+    #   # -----------
+    # # test18: Test different alpha values for rrf with dph
+    # # -----------
+    output_dir = "retrievals/test18"
+    ret_name=output_dir.split("/")[-1]
+    alpha_values = [0.90, 0.91, 0.92, 0.93, 0.94, 0.95, 0.96, 0.97, 0.98, 0.99]
+    num_entry_points = 10000
+    ncandidates = 64
+    max_iter = 16
+
+
+    for alpha in alpha_values:
+
+        metric_dir=f"metrics/{ret_name}/entries/{alpha}"
+
+        retriever.set_parameters(
+            num_entry_points=num_entry_points,
+            ncandidates=ncandidates,
+            max_iter=max_iter,
+            seed_bm25=False,
+            seed_dph=False,
+            rrf_bm25=False,
+            rrf_dph=True,
+            k1=k1,
+            b=b,
+            alpha=alpha,
+        )
+
+        print(f"Starting retrieval: num_entry_points={num_entry_points}, ncandidates={ncandidates}, max_iter={max_iter}, alpha={alpha}")
+        
+        do_retrieval(
+            retriever=retriever,
+            model_name_or_path=model_name_or_path,
+            encoded_item_path=encoded_item_path,
+            item_neighbors_path=item_neighbors_path,
+            output_dir=output_dir,
+            ir_dataset_name=ir_dataset_name,
+            dtype=dtype,
+            num_entry_points=num_entry_points,
+            ncandidates=ncandidates,
+            max_iter=max_iter,
+            cache_file=cache_file,
+            metric_dir=metric_dir
+        )
 
 
     print("Combining metrics.")
